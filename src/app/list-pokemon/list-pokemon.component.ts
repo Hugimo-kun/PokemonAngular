1ecc4233cb9c9bbca8a3c9063dc2aaaf873ff919
Hugimo-kun <hugo.simoneau@gmail.com>
2024-07-30 10:01:23 +0200

    minor updates

--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,22 +1,31 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { IPokemon } from '../../shared/ipokemon';
-import { PokemonService } from '../../shared/pokemon.service';
-import { CardComponent } from '../card/card.component';
+import {CommonModule} from "@angular/common";
+import {Component, inject, OnDestroy, OnInit} from "@angular/core";
+import {IPokemon} from "../../shared/ipokemon";
+import {PokemonService} from "../../shared/pokemon.service";
+import {CardComponent} from "../card/card.component";
 
 @Component({
-  selector: 'app-list-pokemon',
+  selector: "app-list-pokemon",
   standalone: true,
   imports: [CommonModule, CardComponent],
-  templateUrl: './list-pokemon.component.html',
-  styleUrl: './list-pokemon.component.css',
+  templateUrl: "./list-pokemon.component.html",
+  styleUrl: "./list-pokemon.component.css",
 })
-export class ListPokemonComponent implements OnInit {
+export class ListPokemonComponent implements OnInit, OnDestroy {
   pokemons: IPokemon[] = [];
   service = inject(PokemonService);
 
+  count: number = 0;
+  idInterval: any;
+
   ngOnInit() {
     this.getPokemons();
+    this.idInterval = setInterval(() => this.count++, 1000);
+  }
+
+  ngOnDestroy(): void {
+    console.log(this.count);
+    clearInterval(this.idInterval);
   }
 
   getPokemons() {
